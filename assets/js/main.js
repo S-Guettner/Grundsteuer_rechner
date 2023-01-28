5769bc8f51a202be3ffb429ee7bd708d8e9bd9c9
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-28 21:27:16 +0100

    getting data from .json

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,21 @@ fetch("../assets/data/Hebesatz-data.json")
 .then(response => response.json())
 .then(data => gemeindeNamen(data));
 
+
 let gemeindeNamen = (data) => {
-    console.log(data.BB);
-    let test1 = data.BB[1]
-    console.log(test1);
+        
+        let NW = data.NW
+        for(let i = 1; i <= NW.length; i++){
+        console.log(NW[i].Gemeindebezeichnung , NW[i].Column2);
+        }
+/*         let BB = data.BB
+        for(let i = 1; i <= BB.length; i++){
+        console.log(BB[i].Gemeindebezeichnung , BB[i].Column2);
+        } */
 }
 
+/* *TODO: */
+
+/**
+ * TODO: Only first loop in function is working ------- workaround with fetch and function for every array?
+ */
