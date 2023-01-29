67de4c92ec36711de4b51631e5a1016e0b41332f
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-29 18:59:30 +0100

    workaround fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,32 @@
 
 fetch("../assets/data/Hebesatz-data.json")
 .then(response => response.json())
-.then(data => gemeindeNamen(data));
+.then(data => gemeindenNW(data));
 
-
-let gemeindeNamen = (data) => {
+let gemeindenNW = (data) => {
         
         let NW = data.NW
         for(let i = 1; i <= NW.length; i++){
         console.log(NW[i].Gemeindebezeichnung , NW[i].Column2);
         }
-/*         let BB = data.BB
-        for(let i = 1; i <= BB.length; i++){
-        console.log(BB[i].Gemeindebezeichnung , BB[i].Column2);
-        } */
 }
 
-/* *TODO: */
+fetch("../assets/data/Hebesatz-data.json")
+.then(response => response.json())
+.then(data => gemeindenBB(data));
+
+let gemeindenBB = (data) => {
+        
+        let BB = data.BB
+        for(let i = 1; i <= BB.length; i++){
+        console.log(BB[i].Gemeindebezeichnung , BB[i].Column2);
+        }
+}
+
+
+
 
 /**
- * TODO: Only first loop in function is working ------- workaround with fetch and function for every array?
+ * TODO: Only first loop in function is working ------- workaround with fetch and function for every array
+ * TODO: Better solution would be all states in one function
  */
